f3caef7b601535427ebc6dc19904367efb4da477
EduardFierro <edwarfierro9@gmail.com>
2021-08-18 20:41:25 -0500

    archivos

---
+++ src/main/java/co/edu/unicundi/service/CloudinaryService.java
@@ -0,0 +1,47 @@
+package co.edu.unicundi.service;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    Cloudinary cloudinary;
+
+    private Map<String, String> valuesMap = new HashMap<>();
+
+    public CloudinaryService() {
+        valuesMap.put("cloud_name", "dbvsv8u1s");
+        valuesMap.put("api_key", "165832779974686");
+        valuesMap.put("api_secret", "X4vK-bfmsIPWIqRw2UmQFaAhpqo");
+        cloudinary = new Cloudinary(valuesMap);
+    }
+
+    public Map upload(MultipartFile multipartFile) throws IOException {
+        File file = convert(multipartFile);
+        Map result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        file.delete();
+        return result;
+    }
+
+    public Map delete(String id) throws IOException {
+        Map result = cloudinary.uploader().destroy(id, ObjectUtils.emptyMap());
+        return result;
+    }
+
+    private File convert(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        FileOutputStream fo = new FileOutputStream(file);
+        fo.write(multipartFile.getBytes());
+        fo.close();
+        return file;
+    }
+}
